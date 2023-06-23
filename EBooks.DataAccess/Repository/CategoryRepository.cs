f52933043122cf323a14572d4cf062255f381aed
granitbunjaku <granitbunjaku14@gmail.com>
2023-06-23 23:28:09 +0200

    Started using repositories

---
+++ EBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using EBooks.DataAccess.Data;
+using EBooks.DataAccess.Repository.IRepository;
+using EBooks.Models;
+
+namespace EBooks.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private ApplicationDbContext _db;
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        this._db = db;
+    }
+
+    public void Update(Category obj)
+    {
+        _db.Categories.Update(obj);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+}
---
+++ EBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using EBooks.Models;
+
+namespace EBooks.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category obj);
+    void Save();
+}
